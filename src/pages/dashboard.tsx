d622b76116cc6086d5b936e66d8c03cb303846fc
pinya <117656423+postscrippt@users.noreply.github.com>
2026-06-25 22:37:06 +0700

    updated dashboard with mapview

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/navbar.tsx";
+import Map from "../components/MapView.tsx";
 
 function Dashboard() {
     return (
         <div>
             <Navbar />
+            <Map />
         </div>
     )
 }
