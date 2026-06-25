9efade1d02d8de0afd60ceae0649a93d6914db82
boees <80844009+boeingxd@users.noreply.github.com>
2026-06-25 20:52:13 +0700

    Create component directory with MapView.tsx

---
+++ src/components/MapView.tsx
@@ -0,0 +1,33 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix default marker icon broken in Vite
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
+export default function MapView() {
+  return (
+    <MapContainer
+      center={[14.0707, 100.6058]}
+      zoom={15}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors"
+      />
+      <Marker position={[14.0773, 100.5951]}>
+        <Popup>B Dorm Bus Stop</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
