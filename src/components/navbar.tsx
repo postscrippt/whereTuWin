557905fc9d8d6e616099cd379a4e1bf575fffe6f
pinya <117656423+postscrippt@users.noreply.github.com>
2026-06-25 22:15:40 +0700

    stylised navigation button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,29 @@ import logo from "../assets/Logo.svg";
 import "./Navbar.css"
 import search from "../assets/search.svg";
 import hamburg from "../assets/blog-button.svg";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Map", path: "/map" },
+    { name: "About", path: "/about" },
+];
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    function closeMenu() {
+        setIsMenuOpen(false);
+    }
     return (
         <nav className="navbar">
-            <button className="nav-icon-button" aria-label="Open menu">
+            <button className="nav-icon-button" aria-label="Open menu" onClick={toggleMenu}>
                 <img src={hamburg} />
             </button>
 
@@ -17,6 +35,21 @@ function Navbar() {
                     <img src={search} />
                 </span>
             </button>
+
+            {isMenuOpen && (
+                <div className="dropdown-menu">
+                    {navLinks.map((link) => (
+                        <NavLink
+                            key={link.path}
+                            to={link.path}
+                            className="dropdown-link"
+                            onClick={closeMenu}
+                        >
+                            {link.name}
+                        </NavLink>
+                    ))}
+                </div>
+            )}
         </nav>
     );
 }
