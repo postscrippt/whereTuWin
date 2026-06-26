61eef66ef566ff43412653a33a303f4f8d248d41
pinya <117656423+postscrippt@users.noreply.github.com>
2026-06-26 15:44:06 +0700

    Created queueSpots.tsx file to keep queue spot data

---
+++ src/data/queueSpots.tsx
@@ -0,0 +1,11 @@
+export const queueSpots = [
+    {
+        id: 1,
+        name: "TU Main Gate Win",
+        area: "Thammasat Rangsit",
+        landmark: "Main Gate",
+        hours: "6:00 AM - 9:00 PM",
+        lat: 14.0687,
+        lng: 100.6031,
+    },
+];
